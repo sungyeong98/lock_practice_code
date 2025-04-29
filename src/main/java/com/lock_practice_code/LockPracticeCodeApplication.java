88fe77427d7608af6c301e267bae44a1c1b4e6e6
sungyeong98 <am25227903@gmail.com>
2025-04-29 16:57:30 +0900

    feat: 기초 세팅

---
+++ src/main/java/com/lock_practice_code/LockPracticeCodeApplication.java
@@ -0,0 +1,13 @@
+package com.lock_practice_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockPracticeCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockPracticeCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lock_practice_code/LockPracticeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lock_practice_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockPracticeCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
